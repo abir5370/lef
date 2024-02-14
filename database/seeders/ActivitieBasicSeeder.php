729f642acc8984a291edd853426a280e5a7ec0c9
abir5370 <abir537011@gmail.com>
2024-02-14 14:37:54 +0600

    htht

--- database/seeders/ActivitieBasicSeeder.php
+++ database/seeders/ActivitieBasicSeeder.php
@@ -16,14 +16,6 @@ class ActivitieBasicSeeder extends Seeder
         DB::table('basic_activities')->insert([
             'title' => 'Admin',
             'details' => 'Admin fdsfsdfs',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-            'image' => '1244.jpg',
-=======
-            'image' => '125.jpg',
->>>>>>> 8f203068f90332d7767e8919d0df2e8e8c620191
->>>>>>> 957944cc614795f7ec0f64159391920c36741df4
             'gallery_header' => 'Edit gallery_header',
             'vdo_header' => 'Edit vdo_header',
             'vdo_link' => 'Edit vdo_link',
